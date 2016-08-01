fdb5e3fa35b1897e7524513c3757f95c5a86c1e3
Fernanda Ramirez <fernanda.ramirez.olea@gmail.com>
2016-08-01 03:05:13 -0400

    This is working;

--- pluginTester/main.cpp
+++ pluginTester/main.cpp
@@ -1,7 +1,10 @@
 #include "documentwriterplugin.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+
     DocumentWriterPlugin *dwp = new DocumentWriterPlugin();
     QString pluginName = dwp->getName();
 
