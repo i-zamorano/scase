42988f0a3b548719961877730e1d92f75072d389
Andrés Aparicio <adapar@gmail.com>
2016-08-22 11:34:41 -0300

    updated build structure

--- update_working_dir.sh
+++ update_working_dir.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp -f builds/plugins/DocumentWriter/debug/libDocumentWriter.dylib working_dirs/plugins/
-cp -f builds/plugins/DocumentWriter/debug/libDocumentWriter.dylib dist/data_files/plugins/
+cp -f builds/plugins/DocumentWriter/osx/debug/libDocumentWriter.dylib working_dirs/plugins/
+cp -f builds/plugins/DocumentWriter/osx/debug/libDocumentWriter.dylib dist/data_files/plugins/
 ls -la working_dirs/plugins
